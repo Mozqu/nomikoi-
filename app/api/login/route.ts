5099846606a666e6d769c0c8de1889a8722abea8
Mozqu <mys_960408@yahoo.co.jp>
2025-03-22 12:56:16 +0900

    test

--- app/api/login/route.ts
+++ app/api/login/route.ts
@@ -8,7 +8,7 @@ console.log('API Route: Loading...')
 console.log('Environment variables exist:', {
   projectId: !!process.env.FIREBASE_PROJECT_ID,
   clientEmail: !!process.env.FIREBASE_CLIENT_EMAIL,
-  privateKey: !!process.env.FIREBASE_PRIVATE_KEY,
+  privateKey: !!process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
 })
 
 export const runtime = 'nodejs'; // Edge Runtimeを使用しないことを明示
