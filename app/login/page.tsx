9e27f58acbdaa13ee88b71b250629c73616ace28
Mozqu <mys_960408@yahoo.co.jp>
2025-03-28 08:59:45 +0900

    emailLogin

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -34,6 +34,7 @@ export default function LoginPage() {
           'Content-Type': 'application/json',
         },
         body: JSON.stringify({ idToken }),
+        credentials: 'include',
         cache: 'no-store'
       })
       
