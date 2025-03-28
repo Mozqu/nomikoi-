a598542155a800f0f1b2f1ed24fc410018814c31
Mozqu <mys_960408@yahoo.co.jp>
2025-03-28 21:01:19 +0900

    terst

--- app/register/drinking_character/page.tsx
+++ app/register/drinking_character/page.tsx
@@ -449,7 +449,7 @@ export default function DrinkingCharacter() {
         お酒の飲み方診断
       </h1>
       
-      <div className="w-full max-w-md space-y-8 mt-4">
+      <div className="w-full max-w-md space-y-8 mt-4 overflow-y-auto">
         {questionEntries.map(([key, question], index) => (
           <QuestionItem
             key={key}
